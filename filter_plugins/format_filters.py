e2666ce24d624975eabf0c4a18bd7c7fbcbf2f1e
nivekastoreth <jamesmaki@gmail.com>
2018-01-29 14:19:18 -0500

    Add in some stuff about checking for boolean values regardless of if they exist, and also looping with an index

--- filter_plugins/format_filters.py
+++ filter_plugins/format_filters.py
@@ -1,9 +1,39 @@
 
+def format_one(value, fmt):
+    return fmt.format(value)
 
-def format_one(input, format):
-    return format.format(input)
+
+def as_minutes(value_spec):
+    """
+    Utility method for converting strings like '6m' and '8h' into minute values
+    :param value_spec:
+    :return:
+    """
+    def from_spec(spec, value):
+        if spec == 's':
+            return value / 60
+        elif spec == 'm':
+            return value
+        elif spec == 'h':
+            return value * 60
+        elif spec == 'd':
+            return value * 60 * 60
+        else:
+            raise Exception("Unknown duration format: %s. Supported format is <float>[smhd]: 0.5d" % (value_spec))
+    return int(from_spec(
+        value_spec[-1:],
+        float(value_spec[:-1])
+    ))
 
 
 class FilterModule(object):
-    def filters(self):
-        return {'format_one': format_one}
+    @staticmethod
+    def filters():
+        return {
+            'format_one': format_one,
+            'as_minutes': as_minutes
+        }
+
+
+if __name__ == '__main__':
+    as_minutes('bob')
