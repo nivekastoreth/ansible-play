2c4979e1630513a7aba4a8ed5317c4aff1ac2b74
nivekastoreth <jamesmaki@gmail.com>
2018-02-14 13:07:03 -0500

    Add subtask test, tags, and with index tests

--- playbook-diff.sh
+++ playbook-diff.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-: ${PLAYBOOK:="$1"}
 : ${PLAYBOOK:="hello-world"}
 
-ansible-playbook -i localhost "playbook/${PLAYBOOK}/main.yml" --check --diff --connection=local
+ansible-playbook -i localhost "playbook/${PLAYBOOK}/main.yml" --check --diff --connection=local "$@"
